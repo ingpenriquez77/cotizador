ed779ea3cf298ab24957e3bd0d87e21f125a7297
Pedro Rafael Enriquez Nevarez <207899489+pedro-enriquez_coppel@users.noreply.github.com>
2026-07-23 16:57:41 -0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // PERMITIR PROXIES DE RENDER (Soluciona ERR_TOO_MANY_REDIRECTS)
+        // OBLIGATORIO PARA RENDER: Confiar en las cabeceras de HTTPS
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {

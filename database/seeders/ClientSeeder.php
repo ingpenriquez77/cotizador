5fde4125af83d398707359223e6d07f4fcbdf31b
Pedro Rafael Enriquez Nevarez <207899489+pedro-enriquez_coppel@users.noreply.github.com>
2026-07-29 12:20:15 -0700

    Update ClientSeeder.php

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -15,9 +15,9 @@ class ClientSeeder extends Seeder
         Client::updateOrCreate(
             ['business_name' => 'Modelorama Np-Mod Emiliano Zapata'],
             [
-                'contact_name' => 'Juana SArabia',
+                'contact_name' => 'Juana Sarabia',
                 'phone'        => '6677976114',
-                'email'        => 'pedroenriquez81@gmail.cm',
+                'email'        => 'pedroenriquez81@gmail.com',
                 'address'      => 'Blvd. Emiliano Zapata, Culiacán, Sin.',
                 'status'       => 'activo',
             ]
